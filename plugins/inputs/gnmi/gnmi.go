558f28e1ec96579a3568b4c662fb3c7afe055106
door7302 <droy@juniper.net>
2025-01-31 19:58:47 +0100

    f

--- plugins/inputs/gnmi/gnmi.go
+++ plugins/inputs/gnmi/gnmi.go
@@ -400,9 +400,10 @@ func (c *GNMI) handleTelemetryField(update *gnmiLib.Update, tags map[string]stri
 	var jsondata []byte
 
 	// Make sure a value is actually set
-	if update.Val == nil || update.Val.Value == nil {
+	if update.Val.Value == nil {
 		c.Log.Infof("Discarded empty or legacy type value with path: %q", gpath)
 		c.Log.Infof("update.Val: %v", update.Val)
+		c.Log.Infof("update.Val: %t", update.Val)
 		c.Log.Infof("update.Val.Value: %v", update.Val.Value)
 
 		return aliasPath, nil
